eb0489d478d629302051ebb79974e526871283f9
Wendell Cai <caisusandy@gmail.com>
2022-10-27 19:30:03 -0500

    Editor update

---
+++ Editor/UUIDEditor.cs
@@ -0,0 +1,18 @@
+﻿using Amlos.AI;
+using UnityEditor;
+using UnityEngine;
+
+namespace Minerva.Module.Editor
+{
+    [CustomPropertyDrawer(typeof(UUID))]
+    public class UUIDEditor : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            //            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("value"), label);
+            EditorGUI.EndProperty();
+        }
+    }
+}
