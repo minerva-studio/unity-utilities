243c88841d368eb5eed21d3fd53eb226b503a72a
Wendell Cai <caisusandy@gmail.com>
2022-11-27 03:52:45 -0600

    Create ScriptReference class

---
+++ Editor/ScriptReferenceDrawer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Minerva.Module.Editor
+{
+    [CustomPropertyDrawer(typeof(ScriptReference))]
+    [CustomPropertyDrawer(typeof(ScriptReference<>))]
+    public class ScriptReferenceDrawer : PropertyDrawer
+    {
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var r = property.GetValue() as ScriptReference;
+            var file = r.script;
+            var monoScript = (MonoScript)EditorGUI.ObjectField(position, "Type", file, typeof(MonoScript), false);
+            if (monoScript && monoScript.GetClass() != null && (monoScript.GetClass().IsSubclassOf(r.Type) || r.Type == monoScript.GetClass()))
+            {
+                r.script = monoScript;
+                r.SetClass(monoScript.GetClass());
+            }
+            else
+            {
+                r.script = null;
+                r.SetClass(null);
+            }
+            //Debug.Log(monoScript);
+        }
+    }
+    //public class GenericScriptReferenceDrawer : PropertyDrawer
+    //{
+
+    //    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    //    {
+    //        return EditorGUIUtility.singleLineHeight;
+    //    }
+
+    //    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    //    {
+    //        var r = property.GetValue() as ScriptReference;
+    //        var file = r.script;
+    //        var monoScript = (MonoScript)EditorGUI.ObjectField(position, "Type", file, typeof(MonoScript), false);
+    //        r.script = monoScript;
+    //        if (monoScript) r.SetClass(monoScript.GetClass());
+    //        else r.SetClass(null);
+    //        //Debug.Log(monoScript);
+    //    }
+    //}
+}
---
+++ ScriptReference.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace Minerva.Module
+{
+    /// <summary>
+    /// class that refer to an custom script, usable outside editor
+    /// </summary>
+    [Serializable]
+    public class ScriptReference
+    {
+#if UNITY_EDITOR
+        public UnityEditor.MonoScript script;
+#endif
+        public string assemblyQualifiedName = "";
+
+
+        public virtual Type Type => typeof(UnityEngine.Object);
+
+
+        public ScriptReference() { }
+        public ScriptReference(Type type)
+        {
+            SetClass(type);
+        }
+
+        public Type GetClass()
+        {
+            return Type.GetType(assemblyQualifiedName);
+        }
+
+        public void SetClass(Type type)
+        {
+            if (type == null)
+            {
+                assemblyQualifiedName = string.Empty;
+            }
+            else
+            {
+                assemblyQualifiedName = type.AssemblyQualifiedName;
+            }
+        }
+
+        public static implicit operator Type(ScriptReference cr)
+        {
+            return cr.GetClass();
+        }
+
+#if UNITY_EDITOR 
+        public ScriptReference(UnityEditor.MonoScript monoScript)
+        {
+            if (!monoScript)
+            {
+                return;
+            }
+            script = monoScript;
+            SetClass(monoScript.GetClass());
+        }
+
+        public static implicit operator ScriptReference(UnityEditor.MonoScript cr)
+        {
+            return !cr ? new ScriptReference() : new ScriptReference(cr);
+        }
+#endif
+    }
+
+    /// <summary>
+    /// class that refer to an custom script
+    /// </summary>
+    [Serializable]
+    public class ScriptReference<T> : ScriptReference where T : UnityEngine.Object
+    {
+        public override Type Type => typeof(T);
+
+        public ScriptReference() : base() { }
+        public ScriptReference(Type type) : base(type) { }
+
+#if UNITY_EDITOR 
+        public ScriptReference(UnityEditor.MonoScript type) : base(type)
+        {
+        }
+
+        public static implicit operator ScriptReference<T>(UnityEditor.MonoScript cr)
+        {
+            return !cr ? new ScriptReference<T>() : new ScriptReference<T>(cr);
+        }
+
+#endif
+    }
+}
