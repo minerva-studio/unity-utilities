fbdb0cc1ee23ebcddb78ada31059e7911680e7ae
Wendell Cai <caisusandy@gmail.com>
2023-01-03 04:48:14 -0500

    Add RectExtension

---
+++ Extensions/RectExtension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Minerva.Module
+{
+    public static class RectExtension
+    {
+        public static float DistanceTo(this Rect rect, Vector2 position)
+        {
+            var dx = Mathf.Max(rect.min.x - position.x, 0, position.x - rect.max.x);
+            var dy = Mathf.Max(rect.min.y - position.y, 0, position.y - rect.max.y);
+            return Mathf.Sqrt(dx * dx + dy * dy);
+        }
+
+        public static float DistanceTo(this RectInt rect, Vector2 position)
+        {
+            var dx = Mathf.Max(rect.min.x - position.x, 0, position.x - rect.max.x);
+            var dy = Mathf.Max(rect.min.y - position.y, 0, position.y - rect.max.y);
+            return Mathf.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
