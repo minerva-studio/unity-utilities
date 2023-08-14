cdb6aee497261cc09a5da0bca6934b348baa5f69
Wendell Cai <caisusandy@gmail.com>
2023-08-13 20:57:27 -0400

    Add Sorting Layer Editor Attribute

---
+++ Attributes/SortingLayerAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Minerva.Module
+{
+    public class SortingLayerAttribute : PropertyAttribute
+    {
+    }
+}
