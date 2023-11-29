4ae158e0acc31d69d1f7c4fb3a01bc5280c9471e
Wendell Cai <caisusandy@gmail.com>
2023-11-29 01:19:17 -0600

    Add EditorGUIIndent

---
+++ Editor/EditorGUIIndent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+
+namespace Minerva.Module.Editor
+{
+    /// <summary>
+    /// Control GUI enable state
+    /// </summary>
+    public class EditorGUIIndent : IDisposable
+    {
+        private readonly int indent;
+
+        public static EditorGUIIndent Default => new EditorGUIIndent();
+
+        public EditorGUIIndent()
+        {
+            this.indent = 1;
+            EditorGUI.indentLevel += indent;
+        }
+
+        public EditorGUIIndent(int indent)
+        {
+            this.indent = indent;
+            EditorGUI.indentLevel += indent;
+        }
+
+        public static EditorGUIIndent By(int indentation = 1)
+        {
+            return new EditorGUIIndent(indentation);
+        }
+
+        public void Dispose()
+        {
+            EditorGUI.indentLevel -= indent;
+        }
+    }
+}
