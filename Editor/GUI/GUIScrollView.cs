342dafd42a1085f8f5609404630c15a7b09b40db
Wendell Cai <caisusandy@gmail.com>
2024-01-31 05:24:59 -0600

    Horizontal/Vertical/ScrollView helper

---
+++ Editor/GUI/GUIScrollView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Minerva.Module.Editor
+{
+    public class GUIScrollView : IDisposable
+    {
+        public GUIScrollView(ref Vector2 position, params GUILayoutOption[] options)
+        {
+            position = GUILayout.BeginScrollView(position, options);
+        }
+
+        public GUIScrollView(ref Vector2 position, GUIStyle style, params GUILayoutOption[] options)
+        {
+            position = GUILayout.BeginScrollView(position, style, options);
+        }
+
+        public void Dispose()
+        {
+            GUILayout.EndScrollView();
+        }
+    }
+}
---
+++ Editor/GUI/GUIVerticalLayout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Minerva.Module.Editor
+{
+    public class GUIVerticalLayout : IDisposable
+    {
+        public GUIVerticalLayout(params GUILayoutOption[] options)
+        {
+            GUILayout.BeginVertical(options);
+        }
+
+        public GUIVerticalLayout(GUIStyle style, params GUILayoutOption[] options)
+        {
+            GUILayout.BeginVertical(style, options);
+        }
+
+        public void Dispose()
+        {
+            GUILayout.EndVertical();
+        }
+    }
+}
