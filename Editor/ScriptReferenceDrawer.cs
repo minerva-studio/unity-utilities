8f588e9eec62280d5af55215e2fbf86cfca3e38b
Wendell Cai <caisusandy@gmail.com>
2024-02-03 02:53:57 -0600

    Update ScriptReferenceDrawer.cs

--- Editor/ScriptReferenceDrawer.cs
+++ Editor/ScriptReferenceDrawer.cs
@@ -16,18 +16,31 @@ namespace Minerva.Module.Editor
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var r = property.GetValue() as ScriptReference;
-            var file = r.script;
-            var monoScript = (MonoScript)EditorGUI.ObjectField(position, "Type", file, typeof(MonoScript), false);
-            if (monoScript && monoScript.GetClass() != null && (monoScript.GetClass().IsSubclassOf(r.Type) || r.Type == monoScript.GetClass()))
+            var scriptProperty = property.FindPropertyRelative(nameof(ScriptReference.script));
+            EditorGUI.PropertyField(position, scriptProperty);
+
+            //Debug.Log(raw);
+            //Debug.Log(file != null);
+            //Debug.Log(monoScript != null);
+            //if (monoScript && monoScript.GetClass() != null)
+            //{
+            //    Debug.Log(monoScript.GetType());
+            //    Debug.Log(monoScript.name);
+            //    Debug.Log(monoScript.GetClass().Name);
+            //}
+            SerializedProperty aqname = property.FindPropertyRelative(nameof(ScriptReference.assemblyQualifiedName));
+            var monoScript = scriptProperty.boxedValue as MonoScript;
+            if (monoScript && monoScript.GetClass() != null
+                && (monoScript.GetClass().IsSubclassOf(r.Type) || r.Type == monoScript.GetClass())
+                )
             {
-                r.script = monoScript;
-                r.SetClass(monoScript.GetClass());
+                aqname.stringValue = monoScript.GetClass().AssemblyQualifiedName;
             }
             else
             {
-                r.script = null;
-                r.SetClass(null);
+                aqname.stringValue = string.Empty;
             }
+            property.serializedObject.ApplyModifiedProperties();
             //Debug.Log(monoScript);
         }
     }
