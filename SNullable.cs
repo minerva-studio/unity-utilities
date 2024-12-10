102a9202cd9484601e37ec4e14efe8884804bccc
Chad-Vine-Doll <164040662@qq.com>
2024-12-09 23:00:43 -0600

    Update SNullable.cs

--- SNullable.cs
+++ SNullable.cs
@@ -17,28 +17,29 @@ namespace Minerva.Module
         T value;
 
 
-        public bool HasValue => hasValue;
-        public T Value => hasValue ? value : throw new InvalidOperationException();
+        public readonly bool HasValue => hasValue;
+        public readonly T Value => hasValue ? value : throw new InvalidOperationException();
+        public readonly T? Nullable => this;
 
 
 
         public static explicit operator T(SNullable<T> nullable) => nullable.Value;
 
-        public static implicit operator SNullable<T>(T value) => new SNullable<T>() { hasValue = true, value = value };
+        public static implicit operator SNullable<T>(T value) => new() { hasValue = true, value = value };
 
         public static implicit operator T?(SNullable<T> nullable) => nullable.hasValue ? nullable.value : null;
 
-        public static implicit operator SNullable<T>(Nullable<T> nullable) => new SNullable<T>() { hasValue = nullable.HasValue, value = nullable ?? default };
+        public static implicit operator SNullable<T>(T? nullable) => new() { hasValue = nullable.HasValue, value = nullable ?? default };
 
-        public override bool Equals(object other)
+        public override readonly bool Equals(object other)
         {
             if (other is SNullable<T> sn) return sn.HasValue == HasValue && (!HasValue || Value.Equals(sn.Value));
             return other is T n && HasValue && Value.Equals(n);
         }
 
-        public override int GetHashCode() => HasValue ? Value.GetHashCode() : 0;
-        public T GetValueOrDefault() => hasValue ? value : default;
-        public T GetValueOrDefault(T defaultValue) => hasValue ? value : defaultValue;
-        public override string ToString() => HasValue ? "null" : Value.ToString();
+        public override readonly int GetHashCode() => HasValue ? Value.GetHashCode() : 0;
+        public readonly T GetValueOrDefault() => hasValue ? value : default;
+        public readonly T GetValueOrDefault(T defaultValue) => hasValue ? value : defaultValue;
+        public readonly override string ToString() => HasValue ? "null" : Value.ToString();
     }
 }
