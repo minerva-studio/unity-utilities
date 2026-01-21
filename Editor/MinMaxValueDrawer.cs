3933bec643f7cd0c847731d3696f3a0e8a10412f
Caisusandy <164040662@qq.com>
2026-01-21 04:51:31 -0500

    Rework min max value editor

--- Editor/MinMaxValueDrawer.cs
+++ Editor/MinMaxValueDrawer.cs
@@ -13,44 +13,84 @@ namespace Minerva.Module.Editor
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            MinMaxValue minMaxValue;
-            SerializedProperty randomized = property.FindPropertyRelative(nameof(minMaxValue.randomize));
-            SerializedProperty value = property.FindPropertyRelative(nameof(minMaxValue.value));
-            SerializedProperty range = property.FindPropertyRelative(nameof(minMaxValue.range));
-
-            Rect labelRect = position;
-            labelRect.height = EditorGUIUtility.singleLineHeight;
-            labelRect.width = EditorGUIUtility.labelWidth;
-            EditorGUI.LabelField(labelRect, label);
-
-            Rect random = position;
-            random.x += EditorGUIUtility.labelWidth;
-            random.height = EditorGUIUtility.singleLineHeight;
-            var currIndent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-            EditorGUI.PropertyField(random, randomized, new GUIContent("Randomize"));
-            EditorGUI.indentLevel = currIndent;
-
-
-            Rect valRect = position;
-            valRect.height = EditorGUIUtility.singleLineHeight;
-            valRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-            EditorGUI.indentLevel++;
-            if (randomized.boolValue)
+            if (property == null)
             {
-                int indent = EditorGUI.indentLevel * 15;
-                valRect.x += indent;
-                valRect.width -= indent;
-                RangeDrawer.Draw(valRect, range);
-            }
-            else
-            {
-                EditorGUI.PropertyField(valRect, value, true);
+                EditorGUI.LabelField(position, "-");
+                return;
             }
 
-            property.serializedObject.ApplyModifiedProperties();
-            EditorGUI.indentLevel--;
+            var randomizeProp = property.FindPropertyRelative(nameof(MinMaxValue.randomize));
+            var valueProp = property.FindPropertyRelative(nameof(MinMaxValue.value));
+            var rangeProp = property.FindPropertyRelative(nameof(MinMaxValue.range));
 
+            const float toggleWidth = 15f;
+            const float padding = 2f;
+
+            Rect valueRect = new Rect(position.x + padding, position.y, position.width - toggleWidth, position.height - padding * 2);
+            Rect toggleRect = new Rect(valueRect.x + valueRect.width - padding, position.y, toggleWidth, valueRect.height);
+
+            if (randomizeProp != null && randomizeProp.boolValue && rangeProp != null)
+            {
+                var minProp = rangeProp.FindPropertyRelative(nameof(Range.min));
+                var maxProp = rangeProp.FindPropertyRelative(nameof(Range.max));
+
+                if (minProp != null && maxProp != null)
+                {
+                    float halfWidth = (valueRect.width - 15f) * 0.5f;
+                    Rect minRect = new Rect(valueRect.x, valueRect.y, halfWidth, valueRect.height);
+                    Rect separatorRect = new Rect(valueRect.x + halfWidth, valueRect.y, 15f, valueRect.height);
+                    Rect maxRect = new Rect(valueRect.x + halfWidth + 15f, valueRect.y, halfWidth, valueRect.height);
+
+                    minProp.floatValue = EditorGUI.FloatField(minRect, label, minProp.floatValue);
+                    EditorGUI.LabelField(separatorRect, "-", EditorStyles.centeredGreyMiniLabel);
+                    maxProp.floatValue = EditorGUI.FloatField(maxRect, maxProp.floatValue);
+                }
+            }
+            else if (valueProp != null)
+            {
+                valueProp.floatValue = EditorGUI.FloatField(valueRect, label, valueProp.floatValue);
+            }
+
+            if (randomizeProp != null)
+            {
+                DrawModeDropdown(toggleRect, property.serializedObject, randomizeProp, randomizeProp.boolValue);
+            }
+        }
+
+        private static void DrawModeDropdown(Rect dropdownRect, SerializedObject serializedObject, SerializedProperty randomizeProp, bool isRandomized)
+        {
+            // Overlay a clickable hotspot on top of the value field without drawing a background button.
+            // Draw a small arrow to hint interactivity.
+            if (Event.current.type == EventType.Repaint)
+            {
+                EditorStyles.label.Draw(dropdownRect, GUIContent.none, false, false, false, false);
+                GUI.Label(dropdownRect, EditorGUIUtility.IconContent("Icon Dropdown"), EditorStyles.miniLabel);
+            }
+
+            if (GUI.Button(dropdownRect, GUIContent.none, GUIStyle.none))
+            {
+                ShowRandomizationMenu(serializedObject, randomizeProp, isRandomized);
+            }
+        }
+
+        private static void ShowRandomizationMenu(SerializedObject serializedObject, SerializedProperty randomizeProp, bool isRandomized)
+        {
+            GenericMenu menu = new GenericMenu();
+            menu.AddItem(new GUIContent("Constant"), !isRandomized, () => SetRandomized(serializedObject, randomizeProp, false));
+            menu.AddItem(new GUIContent("Random Range"), isRandomized, () => SetRandomized(serializedObject, randomizeProp, true));
+
+            menu.ShowAsContext();
+        }
+
+        private static void SetRandomized(SerializedObject serializedObject, SerializedProperty randomizedProperty, bool enabled)
+        {
+            if (randomizedProperty == null)
+            {
+                return;
+            }
+
+            randomizedProperty.boolValue = enabled;
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
